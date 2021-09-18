a7b2a4c935d4901b779aa3f7efe25b52f8a4f03d
Olga <olga.shvaikovskaya@gmail.com>
2021-09-18 12:32:36 +0300

    Add secret

--- testwebservice/settings.py
+++ testwebservice/settings.py
@@ -1,6 +1,6 @@
 import os
-from django.core.management.utils import get_random_secret_key 
 
+from django.core.management.utils import get_random_secret_key
 from dotenv import load_dotenv
 
 load_dotenv()
